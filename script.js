cdc6bfea9c785adbe3b3d11f511705a0e41bdd72
enl001 <enliven001@mail.ru>
2020-03-16 22:13:37 +0300

    feat: implement singolo-dom task

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let headerMenuLinks = document.querySelectorAll('.header-menu__link');
 let headerMenu = document.querySelector('.header-menu');
 headerMenu.addEventListener('click', (event) => {
   changeSelection(event, 'A', headerMenuLinks, 'header-menu__link_selected');
+
 });
 
 function changeSelection(event, tag, itemsArr, className) {
@@ -38,48 +39,139 @@ let currentSlide = 0;
 let isEnabled = true;
 let buttonPrev = document.querySelector('.slider__button_prev');
 let buttonNext = document.querySelector('.slider__button_next');
-buttonPrev.addEventListener('click', function() {
-  if(isEnabled){
+
+buttonPrev.addEventListener('click', function () {
+  if (isEnabled) {
     previousSlide(currentSlide);
   }
-  
 });
-buttonNext.addEventListener('click', function() {
-  if(isEnabled){
+buttonNext.addEventListener('click', function () {
+  if (isEnabled) {
     nextSlide(currentSlide);
   }
 });
 
-function hideSlide(direction){
+function previousSlide(n) {
+  hideSlide('to-right');
+  changeCurrentSlide(n - 1);
+  showSlide('from-left');
+}
+function nextSlide(n) {
+  hideSlide('to-left');
+  changeCurrentSlide(n + 1);
+  showSlide('from-right');
+}
+function hideSlide(direction) {
   isEnabled = false;
   slides[currentSlide].classList.add(direction);
-  slides[currentSlide].addEventListener('animationend', function() {
+  slides[currentSlide].addEventListener('animationend', function () {
     this.classList.remove('active', direction);
   });
 }
-
-function showSlide(direction){  
+function showSlide(direction) {
   slides[currentSlide].classList.add('next', direction);
-  slides[currentSlide].addEventListener('animationend', function() {
+  slides[currentSlide].addEventListener('animationend', function () {
     this.classList.remove('next', direction);
     this.classList.add('active');
     isEnabled = true;
   });
 }
-
-function previousSlide(n){
-  hideSlide('to-right');
-  changeCurrentSlide(n-1);
-  showSlide('from-left');
-}
-function nextSlide(n){
-  hideSlide('to-left');
-  changeCurrentSlide(n+1);
-  showSlide('from-right');
-}
-function changeCurrentSlide(n){
+function changeCurrentSlide(n) {
   currentSlide = (n + slides.length) % slides.length;
 }
+
+const swiper = (el) => {
+
+  let surface = el;
+  let startX = 0;
+  let startY = 0;
+  let distX = 0;
+  let distY = 0;
+
+  let threshold = 150;
+  let restraint = 100;
+  let allowedTime = 500;
+
+  let startTime = 0;
+  let elapsedTime = 0;
+
+  surface.addEventListener('mousedown', function (e) {
+    startX = e.pageX;
+    startY = e.pageY;
+    startTime = new Date().getTime();
+    e.preventDefault();
+  });
+  surface.addEventListener('mouseup', function (e) {
+    distX = e.pageX - startX;
+    distY = e.pageY - startY;
+    elapsedTime = new Date().getTime() - startTime;
+    if (elapsedTime <= allowedTime) {
+      if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint) {
+        if (distX > 0) {
+          if (isEnabled) {
+            previousSlide(currentSlide);
+          }
+        } else {
+          if (isEnabled) {
+            nextSlide(currentSlide);
+          }
+        }
+      }
+    }
+    e.preventDefault();
+  });
+
+
+  surface.addEventListener('touchmove', function (e) {
+
+    e.preventDefault();
+  });
+  surface.addEventListener('touchstart', function (e) {
+    if (e.target.classList.contains('slider__button')) {
+      if (e.target.classList.contains('slider__button_next')) {
+        if (isEnabled) {
+          nextSlide(currentSlide);
+        }
+      } else if (e.target.classList.contains('slider__button_prev')) {
+        if (isEnabled) {
+          previousSlide(currentSlide);
+        }
+      }
+    }
+
+    let touchObj = e.changedTouches[0];
+    startX = touchObj.pageX;
+    startY = touchObj.pageY;
+    startTime = new Date().getTime();
+    e.preventDefault();
+  });
+  surface.addEventListener('touchend', function (e) {
+    let touchObj = e.changedTouches[0];
+    distX = touchObj.pageX - startX;
+    distY = touchObj.pageY - startY;
+    elapsedTime = new Date().getTime() - startTime;
+    if (elapsedTime <= allowedTime) {
+      if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint) {
+        if (distX > 0) {
+          if (isEnabled) {
+            previousSlide(currentSlide);
+          }
+        } else {
+          if (isEnabled) {
+            nextSlide(currentSlide);
+          }
+        }
+      }
+    }
+    e.preventDefault();
+  });
+
+};
+let el = document.querySelector('.slider-content');
+swiper(el);
+
+
+
 // portfolio
 let portfolioTags = document.querySelectorAll('.button-panel__button');
 let buttonPanel = document.querySelector('.button-panel');
@@ -87,34 +179,37 @@ let images = document.querySelectorAll('.art-album__image');
 let imgContainer = document.querySelector('.art-album');
 
 buttonPanel.addEventListener('click', (event) => {
-  changeSelection(event, 'BUTTON', portfolioTags, 'button-panel__button_selected');
+  
+  if (event.target.tagName === 'BUTTON') {    
+    if (!event.target.classList.contains('button-panel__button_selected')) {
+      for (let btn of portfolioTags) { btn.disabled = true; }
 
-  if (event.target.tagName === 'BUTTON') {
-    for (let btn of portfolioTags) { btn.disabled = true; }
-
-    for (let img of images) {
-      img.classList.remove('art-album__image_selected');
-    }
-
-    let animation = imgContainer.animate([{ opacity: 1 }, { opacity: 0 }],
-      {
-        duration: 700,
-        fill: "forwards"
-      });
-    animation.onfinish = () => {
-      let indexes = getRandomOrderedIndexes(images.length);
-      for (let i = 0; i < images.length; i++) {
-        images[i].style.order = indexes[i];
+      for (let img of images) {
+        img.classList.remove('art-album__image_selected');
       }
-      imgContainer.animate([{ opacity: 1 }, { opacity: 0 }],
+
+      let animation = imgContainer.animate([{ opacity: 1 }, { opacity: 0 }],
         {
-          duration: 700,
-          direction: "reverse",
-          fill: "forwards",
-          delay: 300
+          duration: 500,
+          fill: "forwards"
         });
-      for (let btn of portfolioTags) { btn.disabled = false; }
+      animation.onfinish = () => {
+        let indexes = getRandomOrderedIndexes(images.length);
+        for (let i = 0; i < images.length; i++) {
+          images[i].style.order = indexes[i];
+        }
+        imgContainer.animate([{ opacity: 1 }, { opacity: 0 }],
+          {
+            duration: 500,
+            direction: "reverse",
+            fill: "forwards",
+            delay: 200
+          });
+        for (let btn of portfolioTags) { btn.disabled = false; }
+
+      };
     }
+    changeSelection(event, 'BUTTON', portfolioTags, 'button-panel__button_selected');
   }
 });
 
@@ -139,24 +234,24 @@ let form = document.querySelector('.form');
 let buttonSend = document.querySelector('.form__submit');
 let buttonCloseMessage = document.querySelector('.submit-message__button');
 let modal = document.querySelector('.submit-message');
-form.addEventListener('submit',(event) => {
-  event.preventDefault();  
-  document.querySelector('.body-content').style.overflow = "hidden";
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  //document.querySelector('.body-content').style.overflow = "hidden";
   buttonCloseMessage.focus();
   modal.classList.remove('submit-message_hidden');
   let subj = document.getElementById('subject').value.toString();
-  let subject = (subj !=='')?
-                'Тема: ' + subj : 'Без темы';
+  let subject = (subj !== '') ?
+    'Тема:  ' + subj : 'Без темы';
   let descr = document.getElementById('description').value.toString();
-  let description = (descr !=='')?
-                    'Описание: ' + descr : 'Без описания';
-    
-  document.getElementById('message-subject').innerText = subject; 
-  document.getElementById('message-description').innerText = description; 
-  
+  let description = (descr !== '') ?
+    'Описание:  ' + descr : 'Без описания';
+
+  document.getElementById('message-subject').innerText = subject;
+  document.getElementById('message-description').innerText = description;
+
   buttonCloseMessage.addEventListener('click', () => {
-    document.querySelector('.body-content').style.overflow = "scroll";
+    //document.querySelector('.body-content').style.overflow = "scroll";
     modal.classList.add('submit-message_hidden');
-    form.reset();    
+    form.reset();
   });
 });
